9a20b75f7a6cbb17fbf7bb555273eed7733a8c2e
Asadullah Rifat <anonymusrif@gmail.com>
2017-05-02 22:22:56 +0600

    master commit

--- BulkInvoicePrinter/ViewModel/MainViewModel.cs
+++ BulkInvoicePrinter/ViewModel/MainViewModel.cs
@@ -75,6 +75,8 @@ namespace GOFetchBulkInvoicePrinter.ViewModel
 
         public List<string> JobIDList { get; set; } = new List<string>();
 
+        public List<Job> FetchedJobs { get; set; } = new List<Job>();
+
         #endregion
 
         private RelayCommand _PrintInvoices;
@@ -92,69 +94,76 @@ namespace GOFetchBulkInvoicePrinter.ViewModel
                             JobIDList = new List<string>();
                             JobIDList = JobIDs.Split(' ').ToList();
 
-                            _dataService.GetJob(JobIDs, (res, err) =>
+                            FetchedJobs = new List<Job>();
+
+                            foreach (var JobID in JobIDList)
                             {
-                                if (err != null)
-                                {
-                                    MessageBox.Show(err.Message, "Error!"); return;
-                                }
+                                _dataService.GetJob(JobID, (res, err) =>
+                                                   {
+                                                       if (err != null)
+                                                       {
+                                                           MessageBox.Show(err.Message, "Error!"); return;
+                                                       }
 
-                                if (res != null)
-                                {
-                                    try
-                                    {
-                                        Job job = new Job()
-                                        {
-                                            TrackingNo = (string)res.SelectToken("HRID"),
-                                            ReferenceInvoiceId = (string)res.SelectToken("Order.ReferenceInvoiceId"),
-                                            Date = DateTime.Today.ToLongDateString(),
-                                            PaymentMethod = (string)res.SelectToken("PaymentMethod"),
-                                            Assets = JObject.Parse(res["Assets"].ToString()),
-                                            Tasks = res.SelectToken("Tasks").ToArray(),
-                                        };
+                                                       if (res != null)
+                                                       {
+                                                           try
+                                                           {
+                                                               Job job = new Job()
+                                                               {
+                                                                   TrackingNo = (string)res.SelectToken("HRID"),
+                                                                   ReferenceInvoiceId = (string)res.SelectToken("Order.ReferenceInvoiceId"),
+                                                                   Date = DateTime.Today.ToLongDateString(),
+                                                                   PaymentMethod = (string)res.SelectToken("PaymentMethod"),
+                                                                   Assets = JObject.Parse(res["Assets"].ToString()),
+                                                                   Tasks = res.SelectToken("Tasks").ToArray(),
+                                                               };
 
-                                        string tasks1, tasks2, tasks3 = string.Empty;
+                                                               string tasks1, tasks2, tasks3 = string.Empty;
 
-                                        tasks1 = (string)job.Tasks[1].SelectToken("AssetRef");
-                                        tasks2 = (string)job.Tasks[2].SelectToken("AssetRef");
+                                                               tasks1 = (string)job.Tasks[1].SelectToken("AssetRef");
+                                                               tasks2 = (string)job.Tasks[2].SelectToken("AssetRef");
 
-                                        if (job.Tasks.Count() > 3)
-                                        {
-                                            tasks3 = (string)job.Tasks[3].SelectToken("AssetRef");
-                                        }
+                                                               if (job.Tasks.Count() > 3)
+                                                               {
+                                                                   tasks3 = (string)job.Tasks[3].SelectToken("AssetRef");
+                                                               }
 
-                                        Dictionary<string, string> assetnames = new Dictionary<string, string>();
-                                        foreach (var item in job.Assets)
-                                        {
-                                            assetnames.Add((string)item.Key, (string)item.Value.SelectToken("UserName"));
-                                        }
+                                                               Dictionary<string, string> assetnames = new Dictionary<string, string>();
+                                                               foreach (var item in job.Assets)
+                                                               {
+                                                                   assetnames.Add((string)item.Key, (string)item.Value.SelectToken("UserName"));
+                                                               }
 
-                                        job.PickupBy = assetnames.FirstOrDefault(x => x.Key == tasks1).Value;
-                                        job.DeliveryBy = assetnames.FirstOrDefault(x => x.Key == tasks2).Value;
-                                        job.CashDeliveryBy = job.Tasks.Count() > 3 ? assetnames.FirstOrDefault(x => x.Key == tasks3).Value : "";
+                                                               job.PickupBy = assetnames.FirstOrDefault(x => x.Key == tasks1).Value;
+                                                               job.DeliveryBy = assetnames.FirstOrDefault(x => x.Key == tasks2).Value;
+                                                               job.CashDeliveryBy = job.Tasks.Count() > 3 ? assetnames.FirstOrDefault(x => x.Key == tasks3).Value : "";
 
-                                        job.PickupTime = (string)job.Tasks[1].SelectToken("ETA");
-                                        job.PickupAddress = (string)res.SelectToken("Order.From.AddressLine1");
+                                                               job.PickupTime = (string)job.Tasks[1].SelectToken("ETA");
+                                                               job.PickupAddress = (string)res.SelectToken("Order.From.AddressLine1");
 
-                                        job.ServiceCharge = (string)res.SelectToken("Order.OrderCart.ServiceCharge");
-                                        job.TotalToPay = (string)res.SelectToken("Order.OrderCart.TotalToPay");
-                                        job.SpecialNotetoDeliveryMan = (string)res.SelectToken("Order.NoteToDeliveryMan");
-                                        job.Order = JObject.Parse(res["Order"].ToString());
-                                        job.OrderCart = JObject.Parse(job.Order["OrderCart"].ToString());
-                                        job.PackageList = job.OrderCart.SelectToken("PackageList").ToArray();
+                                                               job.ServiceCharge = (string)res.SelectToken("Order.OrderCart.ServiceCharge");
+                                                               job.TotalToPay = (string)res.SelectToken("Order.OrderCart.TotalToPay");
+                                                               job.SpecialNotetoDeliveryMan = (string)res.SelectToken("Order.NoteToDeliveryMan");
+                                                               job.Order = JObject.Parse(res["Order"].ToString());
+                                                               job.OrderCart = JObject.Parse(job.Order["OrderCart"].ToString());
+                                                               job.PackageList = job.OrderCart.SelectToken("PackageList").ToArray();
 
-                                        job.DeliveryTime = (string)job.Tasks[2].SelectToken("ETA");
-                                        job.DeliveryAddress = (string)res.SelectToken("Order.To.AddressLine1");
-                                        job.PackageList = job.OrderCart.SelectToken("PackageList").ToArray();
+                                                               job.DeliveryTime = (string)job.Tasks[2].SelectToken("ETA");
+                                                               job.DeliveryAddress = (string)res.SelectToken("Order.To.AddressLine1");
+                                                               job.PackageList = job.OrderCart.SelectToken("PackageList").ToArray();
 
-                                    }
-                                    catch (System.Exception ex)
-                                    {
-                                        MessageBox.Show(ex.Message, "Error parsing job!"); return;
-                                    }
-                                }
+                                                               FetchedJobs.Add(job);
 
-                            });
+                                                           }
+                                                           catch (System.Exception ex)
+                                                           {
+                                                               MessageBox.Show(ex.Message, "Error parsing job!"); return;
+                                                           }
+                                                       }
+
+                                                   });
+                            }
                         }
 
                     }));
